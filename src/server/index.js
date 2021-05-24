f961249e8ccf640db0d4b21f8165286c97a9d939
DinaTaklit <ez_taklit@esi.dz>
2021-05-24 18:15:28 +0100

    feat: server: API Call to get rover's information + most recent images taken by that selcted rover

--- src/server/index.js
+++ src/server/index.js
@@ -12,9 +12,8 @@ app.use(bodyParser.json())
 
 app.use('/', express.static(path.join(__dirname, '../public')))
 
-// your API calls
 
-// example API call
+// API Call to get a photo of the day
 app.get('/apod', async (req, res) => {
     try {
         let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
@@ -22,6 +21,29 @@ app.get('/apod', async (req, res) => {
         res.send({ image })
     } catch (err) {
         console.log('error:', err);
+    }
+})
+
+// API Call to get rover's information + most recent images taken by that selcted rover
+app.get('/rovers/:roverName', async (req, res) => {
+    try {
+        const roverName = req.params.roverName; // get the rover name from the request
+
+        // Mission Manifest: will list details of the Rover's mission to help narrow down photo queries to the API. 
+        const missionManifest = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${roverName}?api_key=${process.env.API_KEY}`)
+            .then(res => res.json())      
+            .catch(err => console.log('error:', err));    
+         
+        // Get rover' photos using max_sol from the mission manifest
+        const photos = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?sol=${missionManifest.photo_manifest.max_sol}&api_key=${process.env.API_KEY}`)
+            .then(res => res.json())        
+            .catch(err => console.log('error:', err));    
+        
+        // return the photos
+        res.send(photos.photos)
+
+    } catch(err) {
+        console.error('Oops! something went wrong!', err);
     }
 })
 
